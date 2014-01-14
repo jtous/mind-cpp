8fa5f4570ae2cf78d83ee434a79552572e3d80bd
Seyvoz Stéphane <stephaneseyvoz@gmail.com>
2014-01-14 19:14:26 +0100

    Initial content.

---
+++ adl-backend-cpp/src/main/java/org/ow2/mind/adl/ADLCPPModule.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2014 Schneider-Electric
+ *
+ * This file is part of "Mind Compiler" is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: mind@ow2.org
+ *
+ * Authors: Stephane Seyvoz
+ * Contributors: 
+ */
+
+package org.ow2.mind.adl;
+
+import org.ow2.mind.inject.AbstractMindModule;
+
+public class ADLCPPModule extends AbstractMindModule {
+
+  /**
+   * TODO
+   * 
+   * @see ADLBackendModule from adl-backend
+   * @see ADLStaticBackendModule from adl-backend-static
+   * @see src/main/resources/mind-plugin.xml for plugin configuration
+   */
+
+}
